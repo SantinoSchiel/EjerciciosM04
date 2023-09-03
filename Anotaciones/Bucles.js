6e95eaed57d8ad8800d62259e998005301619056
SantinoSchiel <143773471+SantinoSchiel@users.noreply.github.com>
2023-09-03 16:44:32 -0300

    HomeworkM04

---
+++ Anotaciones/Bucles.js
@@ -0,0 +1,22 @@
+var suma = 0;
+//suma = suma + 1;
+//suma = suma + 2;
+//suma = suma + 3;
+//suma = suma + 4;
+
+//console.log(suma);
+
+/* for(var i=0 ; i<5 ; i++){
+    suma = suma + i;
+    console.log('Variable de iteracion: ', i);
+}
+
+console.log('Variable suma: ', suma); */
+
+/* while(suma < 3){
+    suma = suma + 1;
+    console.log(suma);
+} */
+
+"ENTONCES utilizamos (for) cuando sabemos la cantidad maxima exacta de pasos que queremos ejecutar"
+"Por otro lado utiliziamos (while) cuando no sabemos con certeza cuantos pasos necesitamos hasta finalizar la ejecucion"
